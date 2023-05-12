580373981a0c2b27578d415a5a780b1f08d55d0c
Петя Sit <petia.cyrnicov@gmail.com>
2023-05-12 12:01:34 +0300

    начало

---
+++ bot.php
@@ -0,0 +1,69 @@
+<?php
+//"https://api.telegram.org/bot5974205574:AAGObwmuI1cS0RLadnYysRsOi1sGxz9pbuQ/setWebhook?url=https://573a-219-100-37-243.ngrok-free.app/bot.php "
+
+
+require 'vendor\autoload.php';
+
+use TelegramBot\Api\Client;
+use TelegramBot\Api\Types\Update;
+
+$botToken = "5974205574:AAGObwmuI1cS0RLadnYysRsOi1sGxz9pbuQ";
+
+try {
+   $bot = new Client($botToken);
+
+   $bot->on(function ($update) use ($bot) {
+      $message = $update->getMessage();
+      $chatId = $message->getChat()->getId();
+      $text = $message->getText();
+
+      $characterCount = mb_strlen($text);  // Получаем количество символов в сообщении
+
+      $user = $message->getFrom();
+      $userId = $user->getId();
+      $firstName = $user->getFirstName();
+      $lastName = $user->getLastName();
+      $username = $user->getUsername();
+
+      // Читаем идентификаторы чата из файла
+      $chats = file_exists('chats.txt') ? explode("\n", file_get_contents('chats.txt')) : [];
+
+      if (!in_array($chatId, $chats)) {
+         // Добавляем идентификатор чата в файл, если его там еще нет
+         file_put_contents('chats.txt', $chatId . "\n", FILE_APPEND);
+      }
+
+      if (!in_array($chatId, $chats)) {
+         $response = 'Привет, ' . $firstName . ' ' . $lastName . ' (' . $username . ')!' .
+            ' Твое сообщение содержит ' . $characterCount . ' символов.' .
+            ' Теперь вы можете общаться с ботом.';
+      } else {
+         $response = 'Привет, ' . $firstName . ' ' . $lastName . ' (' . $username . ')!' .
+            ' Твое сообщение содержит ' . $characterCount . ' символов.' .
+            ' Продолжайте общаться с ботом.' . $chatId;
+      }
+
+      $bot->sendMessage($chatId, $response);
+
+      // Отправляем ответное сообщение другому пользователю
+      foreach ($chats as $userChatId) {
+         if ($userChatId !== $chatId && trim($userChatId) != '') {
+            $bot->sendMessage($userChatId, $firstName . ' ' . $lastName . ' написал(а): ' . $chatId . $text);
+         }
+      }
+   }, function () {
+      return true;
+   });
+
+   $bot->run();
+} catch (Exception $e) {
+   file_put_contents('error_log.txt', $e->getMessage());  // Записываем любые исключения в файл журнала ошибок
+}
+
+
+
+
+
+// $bot = new \TelegramBot\Api\BotApi('5974205574:AAGObwmuI1cS0RLadnYysRsOi1sGxz9pbuQ');
+
+// $bot->sendMessage($chatId, $messageText);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'telegram-bot/api' => array(
+            'pretty_version' => 'v2.3.26',
+            'version' => '2.3.26.0',
+            'reference' => '58897c23420a678ab2debef7a73834b54c14e796',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../telegram-bot/api',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
